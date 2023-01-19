34d4261dca8bafc366c68e512eb044472c3d3dc3
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-01-19 11:11:15 -0300

    class 01 mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,59 @@
+// StyleSheet, Text, View são APIs que o React Native disponibiliza para utilizar na aplicação
+// A View é utilizada para declarar o que seria uma div na web, a Text para declarar que quer renderizar texto
+// Depois será renderizado utilizando a API nativa de acordo com o ambiente, seja no Android ou no iOS em que a aplicação estiver rodando
+// StatusBar é uma API que consegue manipular a aparência dos elementos que compõe a StatusBar do dispositivo
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+// useFonts para lidar com o carregamento das fontes
+import {
+  useFonts, 
+  Inter_400Regular, 
+  Inter_600SemiBold, 
+  Inter_700Bold, 
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter';
+
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+  // fontsLoaded para garantir que a aplicação carregue as fontes antes do app ser exibido para o usuário
+  // fontsLoaded é um valor booleano que vai dizer se a fonte está carregada ou não no dispositivo
+  // como useFonts é um hook, se o valor de fontsLoaded mudar, vai notificar
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_800ExtraBold
+  });
+
+  // Se a fonte não está disponível, não renderizar e exibir o conteúdo do app
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    );
+  }
+
+  // O React Native utiliza o React para desenvolver as interfaces de forma declarativa utilizando a sintaxe do JSX
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </View>
+  );
+  // translucent para a tela começar do início da tela do dispositivo e a StatusBar ficar flutuando em cima da aplicação
+}
+
+// A estilização do React Native se dá através do flexbox e das propriedades já conhecidas do CSS para web
+// API de StyleSheet para criar um objeto e dentro dele informar as regras de estilização que deseja aplicar
+// No React Native, o flex já é ativo por padrão
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#09090A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#FFF',
+    fontFamily: 'Inter_800ExtraBold'
+  }
+});
---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import { ActivityIndicator, View } from "react-native";
+
+export function Loading(){
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#09090A' }}>
+            <ActivityIndicator color="#7C3AED" />
+        </View>
+        // <ActivityIndicator /> é um spinner para exibir um loading
+    );
+}
